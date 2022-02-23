c766a4b85b23355229104b62b2fddad2a38719fa
ttommmy <93518302+ttommmy@users.noreply.github.com>
2022-02-23 11:56:37 +0100

    Created tests to check illegal coin values

--- src/test/TestIllegalCoin.java
+++ src/test/TestIllegalCoin.java
@@ -1,8 +1,13 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.*;
 
 import controller.*;
+import model.Currency;
 
 /**
  * Inspired by the book: Flexible, Reliable Software Henrik B�rbak Christensen:
@@ -26,11 +31,68 @@ public class TestIllegalCoin {
 	// Norwegian coin
 	@Test(expected = IllegalCoinException.class)
 	public void shouldRejectIllegalCurrencyNokCoin() throws IllegalCoinException {
-		//
+		//Arrange
+		int coinValue = 1;
+		
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.NOK;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
+		//Act
+		ps.addPayment(coinValue, coinCurrency, coinType);
+		
+		//Assert
+		assertEquals(IllegalCoinException.class, ps.readDisplay());
 	}
 	// unknown Euro coin value
-	@Test(expected = IllegalCoinException.class)
+	@Test
 	public void shouldRejectIllegalEuroCoin() throws IllegalCoinException {
-		//
+		//Arrange
+		int coinValue = 3;
+		boolean found = false;
+		
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.EURO;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
+		
+		//Act
+		try {
+			ps.addPayment(coinValue, coinCurrency, coinType);
+			assertFalse(found);
+		}catch(IllegalCoinException e) {
+			found = true;
+			assertTrue(found);
+		}
+		
+		//Assert
+		
+	}
+	
+	
+	// unknown Euro coin value
+	@Test(expected = IllegalCoinException.class)
+	public void shouldRejectIllegalEuroCoinPartTwo() throws IllegalCoinException {
+		//Arrange
+		int coinValue = 3;
+		
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.EURO;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
+		//Act
+		ps.addPayment(coinValue, coinCurrency, coinType);
+		
+		//Assert
+		assertEquals(IllegalCoinException.class, ps.readDisplay());
+	}
+	
+	// unknown Euro coin value
+	@Test
+	public void shouldNotRejectEuroCoin() throws IllegalCoinException {
+		//Arrange
+		int coinValue = 2;
+		
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.EURO;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
+		//Act
+		ps.addPayment(coinValue, coinCurrency, coinType);
+		
+		//Assert
+		assertEquals(coinValue, ps.readDisplay());
 	}
 }
